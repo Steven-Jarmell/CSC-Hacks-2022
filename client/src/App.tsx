982d349b5c0593c4f76cc8f5098b6f0d14375cee
Steven Jarmell <sjj27@pitt.edu>
2022-11-07 16:58:59 -0500

    Cleanup App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
 type JobEntry = {
-  companyName: string,
-	link: string,
-  locations: string[],
-	description: string,
-	sponsorship: string,
-	status: boolean
-}
+	companyName: string;
+	link: string;
+	locations: string[];
+	description: string;
+	sponsorship: string;
+	status: boolean;
+};
 
 function App() {
 	const [backendData, setBackendData] = useState<JobEntry[]>([]);
@@ -21,68 +21,82 @@ function App() {
 				return response.text();
 			})
 			.then((data) => {
-        // Gets just the job posting text
+				// Gets just the job posting text
 				let splitData = data.substring(data.indexOf("Akuna") - 1);
 				splitData = splitData.substring(0, splitData.indexOf("<!--") - 4);
 
-        // Split the text at the '|'
-        let filteredData: string[] = splitData.split('|');
+				// Split the text at the '|'
+				let filteredData: string[] = splitData.split("|");
 
-        // Make a list of all the jobs
-        let allJobs = [];
-        for (let i = 0; i < filteredData.length; i+=3) {
-          if (filteredData[i] === '\n') {
-            i++;
-          }
+				// Make a list of all the jobs
+				let allJobs = [];
 
-          let curEntry:JobEntry = {} as JobEntry;
-          
-          if (filteredData[i].indexOf('[') !== -1 && filteredData[i].indexOf(']') !== -1) {
-            curEntry.companyName = filteredData[i].substring(filteredData[i].indexOf('[')+1, filteredData[i].indexOf(']'));
-            curEntry.link = filteredData[i].substring(filteredData[i].indexOf('(')+1, filteredData[i].toLowerCase().indexOf(')'));
-            curEntry.status = true;
-          } else {
-            curEntry.companyName = filteredData[i];
-            curEntry.link = '';
-            curEntry.status = false;
-          }
-          
-          curEntry.locations = filteredData[i+1].split(';');
+				// Parse through the filtered data
+				for (let i = 0; i < filteredData.length; i += 3) {
+					// If the line is blank, skip it
+					if (filteredData[i] === "\n") {
+						i++;
+					}
 
-          let description:string = filteredData[i+2];
-          curEntry.description = description;
+					let curEntry: JobEntry = {} as JobEntry;
 
-          let lowerCaseDescription = description.toLowerCase();
-          if (lowerCaseDescription.includes('sponsorship') && !lowerCaseDescription.includes('not') && !lowerCaseDescription.includes('no')) {
-            curEntry.sponsorship = "Available";
-          } else {
-            curEntry.sponsorship = "Unavailable/Unknown";
-          }
+					// Check if the job is closed
+					// If it is closed there is no link attached to it
+					if (
+						filteredData[i].indexOf("[") !== -1 &&
+						filteredData[i].indexOf("]") !== -1
+					) {
+						curEntry.companyName = filteredData[i].substring(
+							filteredData[i].indexOf("[") + 1,
+							filteredData[i].indexOf("]")
+						);
+						curEntry.link = filteredData[i].substring(
+							filteredData[i].indexOf("(") + 1,
+							filteredData[i].toLowerCase().indexOf(")")
+						);
+						curEntry.status = true;
+					} else {
+						curEntry.companyName = filteredData[i];
+						curEntry.link = "";
+						curEntry.status = false;
+					}
 
-          allJobs.push(curEntry);
-        }
+					// Location tends to be a semi-colon separated list
+					// Need to revise this in the future to account for anomolies
+					curEntry.locations = filteredData[i + 1].split(";");
 
-        setBackendData(allJobs);
-        //setBackendData(filteredData);
+					// Description occurs after location
+					let description: string = filteredData[i + 2];
+					curEntry.description = description;
+
+					let lowerCaseDescription = description.toLowerCase();
+
+					// Naiive way to determine sponsorship or not
+					if (
+						lowerCaseDescription.includes("sponsorship") &&
+						!lowerCaseDescription.includes("not") &&
+						!lowerCaseDescription.includes("no")
+					) {
+						curEntry.sponsorship = "Available";
+					} else {
+						curEntry.sponsorship = "Unavailable/Unknown";
+					}
+
+					// Add job to the list
+					allJobs.push(curEntry);
+				}
+
+				setBackendData(allJobs);
 			});
 	}, []);
 
-	// [Name Of Company](link to company)? | Location | Description
-	/*
-  return (
-    <div>
-      {backendData ? (
-        <p>{backendData}</p>
-      ) : (
-        "Loading..."
-      )}
-    </div>
-  );
-  */
-return <div>{backendData ? backendData.map((entry, i) => (
-    <p key={i}>{JSON.stringify(entry)}</p>
-    //<p key={i}>{i}: {entry}</p>
-  )) : "Loading..."}</div>;
+	return (
+		<div>
+			{backendData
+				? backendData.map((entry, i) => <p key={i}>{JSON.stringify(entry)}</p>)
+				: "Loading..."}
+		</div>
+	);
 }
 
 export default App;
