6e91067b80ac7f1a49f34e7673e8d0e23fadd9d3
Steven Jarmell <sjj27@pitt.edu>
2022-11-08 17:26:30 -0500

    Fix parser to catch more edge cases Display JobPosting Components to screen instead of JSON Add horribly written CSS Change JobPosting Component to not use a list but just display locations as they are written

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+import JobPosting from "./Components/JobPosting";
 import { JobEntry } from "./Types/JobEntry";
+import "./App.css";
 
 function App() {
 	const [backendData, setBackendData] = useState<JobEntry[]>([]);
@@ -26,11 +28,37 @@ function App() {
 				// Parse through the filtered data
 				for (let i = 0; i < filteredData.length; i += 3) {
 					// If the line is blank, skip it
-					if (filteredData[i] === "\n") {
+					while (
+						filteredData[i] === "\n" ||
+						filteredData[i].trim().length === 0
+					) {
 						i++;
 					}
 
 					let curEntry: JobEntry = {} as JobEntry;
+
+					// Special case that needs hard coding to deal with
+					if (filteredData[i].includes("Jump Trading")) {
+						curEntry.companyName = "Jump Trading";
+						curEntry.description =
+							filteredData[i + 2] +
+							filteredData[i + 3] +
+							filteredData[i + 4] +
+							filteredData[i + 5] +
+							filteredData[i + 6];
+						curEntry.link = filteredData[i].substring(
+							filteredData[i].indexOf("(") + 1,
+							filteredData[i].toLowerCase().indexOf(")")
+						);
+						curEntry.sponsorship = "Unavailable/Unknown";
+						curEntry.status = true;
+						curEntry.locations = "Chicago, IL";
+
+						allJobs.push(curEntry);
+
+						i += 6;
+						continue;
+					}
 
 					// Check if the job is closed
 					// If it is closed there is no link attached to it
@@ -55,7 +83,7 @@ function App() {
 
 					// Location tends to be a semi-colon separated list
 					// Need to revise this in the future to account for anomolies
-					curEntry.locations = filteredData[i + 1].split(";");
+					curEntry.locations = filteredData[i + 1];
 
 					// Description occurs after location
 					let description: string = filteredData[i + 2];
@@ -85,7 +113,20 @@ function App() {
 	return (
 		<div>
 			{backendData
-				? backendData.map((entry, i) => <p key={i}>{JSON.stringify(entry)}</p>)
+				? backendData.map((entry, i) => (
+						<div key={i}>
+							{
+								<JobPosting
+									companyName={entry.companyName}
+									link={entry.link}
+									status={entry.status}
+									locations={entry.locations}
+									sponsorship={entry.sponsorship}
+									description={entry.description}
+								/>
+							}
+						</div>
+				  ))
 				: "Loading..."}
 		</div>
 	);
--- client/src/Components/JobPosting.tsx
+++ client/src/Components/JobPosting.tsx
@@ -1,4 +1,5 @@
 import { JobEntry } from "../Types/JobEntry"
+import './JobPosting.css'
 
 const JobPosting = ({
 	companyName,
@@ -12,26 +13,14 @@ const JobPosting = ({
 		<div className="job-posting-container">
 			{/* Add header with a link to job if available, else just list job name */}
 			<div className="job-posting-header">
-				{status && <a href={link}>{companyName}</a>}
-				{!status && <p>{companyName}</p>}
+				{status && <a className="job-posting-company" href={link}>{companyName}</a>}
+				{!status && <p className="job-posting-company">{companyName}</p>}
 			</div>
 			<div className="job-posting-body">
-                {/* If theres more than 1 location, map through the list, else display the one*/}
-				{locations.length > 1 ? (
-                    <div>
-                        <p>Locations:</p>
-                        <ul className="job-posting-list">
-                            {locations.map((location, i) => (
-                                <li key={i}>{location}</li>
-                            ))}
-                        </ul>
-                    </div>
-				) : (
-					<p>Location: {locations[0]}</p>
-				)}
-				<p>Job Description: {description}</p>
-				<p>Sponsorship: {sponsorship}</p>
-				<p>Status: {status ? "Open" : "Closed"}</p>
+				<p><span className="job-posting-subtitle">Location:</span> {locations}</p>
+				<p><span className="job-posting-subtitle">Job Description:</span> {description}</p>
+				<p><span className="job-posting-subtitle">Sponsorship:</span> {sponsorship}</p>
+				<p><span className="job-posting-subtitle">Status:</span> {status ? <span className="job-posting-status-open">Open</span> : <span className="job-posting-status-closed">Closed</span>}</p>
 			</div>
 		</div>
 	);
