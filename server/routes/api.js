05055b636ea15d3c2afb5063868d276276cafaa6
Jonah Osband <79324007+josband@users.noreply.github.com>
2022-11-15 10:53:52 -0500

    Minor bug fixes before merge with main

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,5 @@
 import express from "express";
 import Job from "../models/job.js";
-import parseDocument from "../util/parseDocument.js";
 const router = express.Router();
 
 router.route("/jobs").get((req, res) => {
@@ -9,42 +8,6 @@ router.route("/jobs").get((req, res) => {
     .then((jobs) => res.json(jobs))
     .catch((err) => res.status(400).json("Error" + err));
 });
-
-// router.route("/update").get(async (req, res) => {
-//   // Delete data from database
-// 	Job.deleteMany()
-// 		.then(console.log("Data Deleted"))
-// 		.catch((err) => console.log(error));
-
-//   // Parse READAME and get list of jobs
-//   let jobList = await parseDocument();
-
-//   // Add jobs to database
-//   for (let curJob of jobList) {
-//     let companyName = curJob.companyName;
-//     let link = curJob.link ? curJob.link : '';
-//     let status = curJob.status;
-//     let locations = curJob.locations;
-//     let description = curJob.description;
-//     let sponsorship = curJob.sponsorship;
-
-//     const newJob = new Job({
-//       companyName,
-//       link,
-//       locations,
-//       description,
-//       sponsorship,
-//       status,
-//     });
-
-//     newJob
-// 		.save()
-// 		.then()
-// 		.catch((err) => res.status(400).json("Error: " + err));
-//   }
-
-//   res.send('hello');
-// });
 
 router.route("/add").post((req, res) => {
   const companyName = req.body.companyName;
